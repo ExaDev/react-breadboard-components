9ba2c7bbba9c8d988fee63519d3abda21fbdbbda
a.mitu <a.mitu@sourcepulp.com>
2024-06-03 13:32:36 +0100

    revert to previous approach for running a sample; wip: investigate and update breadboard runner class

--- src/demos/RunPreview.tsx
+++ src/demos/RunPreview.tsx
@@ -1,10 +1,14 @@
+import { InputValues } from "@google-labs/breadboard";
+import { HarnessRunResult } from "@google-labs/breadboard/harness";
 import { useEffect, useState } from "react";
+import { Events } from "@google-labs/breadboard-ui";
+import { BreadboardInputForm } from "~/src/components";
 import "./RunPreview.css";
 import useDevPulseKits from "./hooks/use-dev-pulse-kits";
-import { BreadboardRunner } from "./breadboard/BreadboardRunner";
-import BreadboardElementRenderer from "./breadboard/breadboard-element-renderer";
-import { HarnessRunResult } from "@google-labs/breadboard/harness";
-import HackerNewsAlgoliaKit from "./breadboard/kits/HackerNewsAlgoliaKit";
+import BreadboardSecretInput from "./breadboard/breadboard-element-renderer/elements/BreadboardSecretInput";
+import BreadboardError from "./breadboard/breadboard-element-renderer/elements/BreadboardError";
+import BreadboardOutput from "./breadboard/breadboard-element-renderer/elements/BreadboardOutput";
+import { runBoard } from "./breadboard/board-runner";
 
 type BbPreviewRunProps = {
 	boardUrl?: string;
@@ -14,26 +18,56 @@ const BbPreviewRun = ({
 	boardUrl = "/graphs/dev-pulse.json",
 }: BbPreviewRunProps): React.JSX.Element => {
 	const kits = useDevPulseKits();
-	const runner = new BreadboardRunner(boardUrl, kits);
-	let runResult: HarnessRunResult = {} as HarnessRunResult;
+	const [uiElement, setUiElement] = useState<React.ReactNode>();
+
+	const handleStateChange = async (
+		result: HarnessRunResult
+	): Promise<void | InputValues> => {
+		switch (result.type) {
+			case "secret": {
+				setUiElement(<BreadboardSecretInput result={result} />);
+				return Promise.resolve(void 0);
+			}
+
+			case "input":
+				return new Promise((resolve) => {
+					setUiElement(
+						<BreadboardInputForm
+							schema={result.data.inputArguments.schema}
+							onSubmit={(event: Events.InputEnterEvent) => {
+								setUiElement(null);
+								resolve(event.data as InputValues);
+							}}
+						/>
+					);
+				});
+
+			case "error":
+				setUiElement(<BreadboardError result={result} />);
+				return Promise.resolve(void 0);
+
+			case "output":
+				setUiElement(
+					<BreadboardOutput
+						output={result.data.outputs}
+						uiElement={uiElement}
+					/>
+				);
+				return Promise.resolve(void 0);
+		}
+	};
+
 	useEffect(() => {
 		if (boardUrl) {
-			runner.runBoard().then((r) => {
-				runResult = runner.getRunResult();
-			});
+			runBoard(boardUrl, kits, handleStateChange);
 		}
-		// runResult = runner.getRunResult();
-	}, [runResult]);
-
-	console.log(runResult);
+	}, []);
 
 	return (
 		<main>
 			<section className="samplesRunnerContainer">
 				<h4>ExaDev Samples</h4>
-				<section>
-					<BreadboardElementRenderer result={runResult} elementName="input" />
-				</section>
+				<section>{uiElement}</section>
 			</section>
 			<footer>Made with Breadboard</footer>
 		</main>
